f7f68e1108ead1e415b21ed9b10faca44596db73
Khenziii <khenzii.was.taken@gmail.com>
2024-03-19 21:20:20 +0100

    feat: make the snake move

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import pygame
 from typing import TypedDict, Tuple, List
+from enum import Enum
+
+
+class Direction(Enum):
+    UP = "up"
+    DOWN = "down"
+    LEFT = "left"
+    RIGHT = "right"
 
 
 class SquareConfig(TypedDict):
@@ -18,10 +26,13 @@ class Square:
         self.color = config["color"]
         self.display = config["display"]
 
-        self.change_color(self.color)
+        self.rerender()
 
     def change_color(self, color: Tuple[int, int, int]):
-        pygame.draw.rect(self.display, color, (self.location_x, self.location_y, self.size, self.size))
+        self.color = color
+
+    def rerender(self):
+        pygame.draw.rect(self.display, self.color, (self.location_x, self.location_y, self.size, self.size))
 
 
 class Position(TypedDict):
@@ -52,6 +63,7 @@ class Game:
 
         self.tiles: List[SquaresType] = []
         self.snake_tiles: List[Position] = []
+        self.snake_direction: Direction = Direction.RIGHT
 
         self.__run()
 
@@ -70,6 +82,8 @@ class Game:
                 if event.type == pygame.QUIT:
                     self.running = False
 
+            self.__render_board()
+            self.__move_snake()
             pygame.display.flip()  # .flip() updates the display
             pygame.time.Clock().tick(self.game_speed)  # FPS cap
 
@@ -119,12 +133,57 @@ class Game:
         else:
             print("WARNING: Tried to un-snake a square that's not a snake, something's probably wrong with the logic")
 
+    def __get_square_by_x_and_y(self, x: int, y: int) -> SquaresType:
+        results = [d for d in self.tiles if d.get("x") == x and d.get("y") == y]
+        return results[0]
+
+    def __render_board(self):
+        for tile in self.tiles:
+            tile["square"].rerender()
+
+    def __move_snake(self):
+        head: Position = self.snake_tiles[-1]
+        tail: Position = self.snake_tiles[0]
+
+        square_to_remove = self.__get_square_by_x_and_y(tail["x"], tail["y"])
+        self.__unset_square_as_snake(square_to_remove)
+
+        match self.snake_direction:
+            case Direction.UP:
+                if head["y"] + 1 >= self.game_grid_size:
+                    square_to_add = self.__get_square_by_x_and_y(head["x"], 0)
+                else:
+                    square_to_add = self.__get_square_by_x_and_y(head["x"], head["y"] + 1)
+
+                self.__set_square_as_snake(square_to_add)
+            case Direction.DOWN:
+                if head["y"] - 1 <= self.game_grid_size:
+                    square_to_add = self.__get_square_by_x_and_y(head["x"], self.game_grid_size)
+                else:
+                    square_to_add = self.__get_square_by_x_and_y(head["x"], head["y"] - 1)
+
+                self.__set_square_as_snake(square_to_add)
+            case Direction.RIGHT:
+                if head["x"] + 1 >= self.game_grid_size:
+                    square_to_add = self.__get_square_by_x_and_y(0, head["y"])
+                else:
+                    square_to_add = self.__get_square_by_x_and_y(head["x"] + 1, head["y"])
+
+                self.__set_square_as_snake(square_to_add)
+            case Direction.LEFT:
+                if head["x"] - 1 <= self.game_grid_size:
+                    square_to_add = self.__get_square_by_x_and_y(self.game_grid_size, head["y"])
+                else:
+                    square_to_add = self.__get_square_by_x_and_y(head["x"] - 1, head["y"])
+
+                self.__set_square_as_snake(square_to_add)
+
 
 gameConfig: GameConfig = {
     "window_size_px": 1000,
     "window_title": "Snake Game",
-    "game_speed": 60,
-    "game_grid_size": 5,
+    "game_speed": 10,
+    "game_grid_size": 20,
     "game_snake_start_length": 5,
 }
 game = Game(config=gameConfig)
