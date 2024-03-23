aac16869fffac65fe14bbdcc7624559aa78e8cdb
Khenzii <khenzii.was.taken@gmail.com>
2024-03-23 23:05:29 +0100

    feat: config (#3)

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from ai.ai_game import GameConfig, AIGame
 from ai.model import NetConfig, Net
 from ai.agent import AgentConfig, Agent
 from ai.trainer import TrainerConfig, Trainer
+from config.config import ConfigConfig, Config
 
 # gameConfig: GameConfig = {
 #     "window_size_px": 1000,
@@ -17,40 +18,25 @@ from ai.trainer import TrainerConfig, Trainer
 # }
 # game = Game(config=gameConfig)
 
-gameConfig: GameConfig = {
-    "window_size_px": 1000,
-    "window_title": "Snake Game",
-    "game_speed": 1_000,
-    "game_grid_size": 10,
-    "game_snake_start_length": 3,
-    "game_apple_start_count": 2,
-    "game_snake_color": (255, 255, 255),
-    "game_apple_color": (255, 0, 0),
-    "game_background_color": (0, 0, 0),
-    "game_auto_handle_loop": False,
-    "game_finish_print": False
+configConfig: ConfigConfig = {
+    "config_file_path": "config.json",
 }
+configManager = Config(configConfig)
+gameConfig: GameConfig = configManager.get_game_config(
+    game_auto_handle_loop=False,
+    game_finish_print=True
+)
 env = AIGame(config=gameConfig)
-netConfig: NetConfig = {
-    "input_nodes": 10 ** 2 * 4,
-    "hidden_nodes": 256,
-    "output_nodes": 4,
-}
+netConfig: NetConfig = configManager.get_net_config()
 net = Net(config=netConfig)
-agentConfig: AgentConfig = {
-    "model": net,
-    "env": env,
-}
+agentConfig: AgentConfig = configManager.get_agent_config(
+    model=net,
+    env=env,
+)
 agent = Agent(config=agentConfig)
-trainerConfig: TrainerConfig = {
-    "model": net,
-    "memory_size": 10_000,
-    "batch_size": 32,
-    "gamma": 0.99,
-    "epsilon_start": 1.0,
-    "epsilon_end": 0.01,
-    "epsilon_decay": 0.995,
-}
+trainerConfig: TrainerConfig = configManager.get_trainer_config(
+    model=net
+)
 trainer = Trainer(config=trainerConfig)
 
 scores = []
