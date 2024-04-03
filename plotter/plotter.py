2527efc8929f30570591b4c285b35214bcaac6e1
Khenziii <khenzii.was.taken@gmail.com>
2024-04-03 17:58:29 +0200

    feat(plotter): add mean line

--- plotter/plotter.py
+++ plotter/plotter.py
@@ -17,9 +17,11 @@ class Plotter:
 
         self.x_data = []
         self.y_data = []
+        self.mean_y = []
 
         self.figure, subplots = plt.subplots()
         self.line, = subplots.plot(self.x_data, self.y_data, '-')
+        self.mean_line, = subplots.plot(self.x_data, self.mean_y, '-')
 
         plt.xlabel(self.x_axis_label)
         plt.ylabel(self.y_axis_label)
@@ -38,8 +40,12 @@ class Plotter:
         self.x_data.append(x)
         self.y_data.append(y)
 
+        self.mean_y.append(sum(self.y_data) / len(self.y_data))
+
     def __update(self, frame):
         self.line.set_data(self.x_data, self.y_data)
+        self.mean_line.set_data(self.x_data, self.mean_y)
+
         self.figure.gca().relim()
         self.figure.gca().autoscale_view()
         return self.figure,
